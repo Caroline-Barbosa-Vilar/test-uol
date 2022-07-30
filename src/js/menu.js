d58858ff44578d525d6f8f14e25d7f53294fa187
Caroline Barbosa <bo.varig@gmail.com>
2022-07-29 21:27:02 -0300

    finished the js animation

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,4 +1,12 @@
-const menu = document.getElementById('menu');
-menu.addEventListener('click', function() {
-  menu.remove();
+const kebab = document.querySelector('.menu');
+const navMenu = document.querySelector('.nav-menu');
+
+kebab.addEventListener('click', () => {
+  kebab.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
+
+document.querySelectorAll('.nav-menu').forEach(n => n.addEventListener('click', () => {
+  kebab.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
