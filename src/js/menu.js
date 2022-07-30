8bc9e1d8e7e65bd9b5f5209853250385d74bb0c2
Caroline Barbosa <bo.varig@gmail.com>
2022-07-30 09:24:39 -0300

    fixed the menu name

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,12 @@
-const kebab = document.querySelector('.menu');
+const bento = document.querySelector('.menu');
 const navMenu = document.querySelector('.nav-menu');
 
-kebab.addEventListener('click', () => {
-  kebab.classList.toggle('active');
+bento.addEventListener('click', () => {
+  bento.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
 document.querySelectorAll('.nav-menu').forEach(n => n.addEventListener('click', () => {
-  kebab.classList.remove('active');
+  bento.classList.remove('active');
   navMenu.classList.remove('active');
 }));
